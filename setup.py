18c8f9127aba0e691b40a3e54f57a01f7e426a03
Matthew Brown <mbrown1508@outlook.com>
2017-03-24 05:47:07 -0700

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='microsoftbotframework',
-      version='0.1.6',
+      version='0.1.7',
       description='A wrapper for the microsoft bot framework API',
       classifiers=[
             'Development Status :: 3 - Alpha',
