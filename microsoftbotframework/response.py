9cf9282d0366e233c64ab5b063ec33782d602c36
Matthew Brown <mbrown1508@outlook.com>
2017-03-27 04:51:22 -0700

    http encoded conversation_id

--- microsoftbotframework/response.py
+++ microsoftbotframework/response.py
@@ -1,6 +1,7 @@
 from .config import Config
 from urllib.parse import urljoin
 import requests
+from requests.utils import quote
 import datetime
 import redis
 import logging
@@ -130,8 +131,9 @@ class Response:
         conversation_id = self['conversation']["id"] if conversation is None else conversation['id']
         reply_to_id = self['id'] if reply_to_id is None else reply_to_id
 
-        response_url = urljoin(self["serviceUrl"], "/v3/conversations/{}/activities/{}".format(conversation_id,
-                                                                                               reply_to_id))
+        response_url = urljoin(self["serviceUrl"], "/v3/conversations/{}/activities/{}".format(
+                                                                            quote(conversation_id, safe=''),
+                                                                            reply_to_id))
 
         response_json = {
             "from": self["recipient"] if from_info is None else from_info,
