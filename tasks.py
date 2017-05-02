909fdde229b1869ee0febb93f081e67a84da7745
Matthew Brown <mbrown1508@outlook.com>
2017-05-02 23:03:25 +1000

    Swapped to using pypi version of PyJWT

--- tasks.py
+++ tasks.py
@@ -15,13 +15,13 @@ def echo_response(message):
         message_response = message["text"]
         response_info = response.reply_to_activity(message_response)
 
-        from time import sleep
+#        from time import sleep
 
-        sleep(2)
-        response.delete_activity(activity_id=response_info.json()['id'])
+#        sleep(2)
+#        response.delete_activity(activity_id=response_info.json()['id'])
 
-        sleep(2)
-        response.create_conversation('lets talk about something really interesting')
+#        sleep(2)
+#        response.create_conversation('lets talk about something really interesting')
 
 
 # This is a asynchronous task
