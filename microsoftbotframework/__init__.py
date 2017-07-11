371e24929d5e59f98243820bceecfa583e20b396
Matthew Brown <mbrown1508@outlook.com>
2017-07-11 19:38:57 +1000

    Added Redis memory object

--- microsoftbotframework/__init__.py
+++ microsoftbotframework/__init__.py
@@ -3,4 +3,4 @@ from .msbot import MsBot
 from .config import Config
 from .activity import Activity
 from .conversationoperations import ReplyToActivity, SendToConversation, DeleteActivity, CreateConversation, GetActivityMembers, GetConversationMembers
-from .memory import JsonMemory
+from .memory import JsonMemory, RedisMemory
--- microsoftbotframework/memory.py
+++ microsoftbotframework/memory.py
@@ -1,5 +1,6 @@
 import os
 import json
+import redis
 
 
 class Memory(object):
@@ -63,4 +64,39 @@ class JsonMemory(Memory):
             data_file.truncate()
 
         return True
+
+
+class RedisMemory(Memory):
+    # currently loading is only from config file
+    def __init__(self, config):
+        Memory.__init__(self)
+
+        self.redis_uri = config.get_config(None, 'URI', root='redis')
+        self.redis = None
+        self.redis_config = config.get_section_config('redis')
+
+    def get(self, key):
+        self._connect()
+        value = self.redis.get(key)
+        if value is not None:
+            value = value.decode('UTF-8')
+        return value
+
+    def set(self, key, value):
+        self._connect()
+        self.redis.set(key, value)
+
+    def delete(self, key):
+        self._connect()
+        self.redis.delete(key)
+
+    def _connect(self):
+        if self.redis is None:
+            self.redis = redis.StrictRedis.from_url(self.redis_uri)
+            for name, value in self.redis_config.items():
+                try:
+                    self.redis.config_set(name, value)
+                except:
+                    pass
+                    # log...
 
