82cebfe236586483b5fef98f60a9a86454ab5dda
Matthew Brown <mbrown1508@outlook.com>
2017-07-22 15:01:49 +1000

    Got port from environment vars

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn heroku:bot
+web: gunicorn heroku:bot -b '0.0.0.0:'$PORT
 celery: celery -A microsoftbotframework.runcelery.celery worker --concurrency=5 --loglevel=info
--- heroku.py
+++ heroku.py
@@ -7,27 +7,26 @@ import celery
 from tasks import *
 from microsoftbotframework.msbot import MsBot
 import os
+import microsoftbotframework.runcelery
+import logging
 
 
-if __name__ == "__main__":
-    import microsoftbotframework.runcelery
-    import logging
-    log_format = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
-    formatter = logging.Formatter(log_format)
+log_format = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
+formatter = logging.Formatter(log_format)
 
-    streamHandler = logging.StreamHandler()
-    streamHandler.setLevel(logging.INFO)
-    streamHandler.setFormatter(formatter)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.INFO)
+streamHandler.setFormatter(formatter)
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format=log_format)
+logging.basicConfig(
+    level=logging.INFO,
+    format=log_format)
 
-    bot = MsBot()
-    bot.add_process(respond_to_conversation_update)
-    bot.add_process(synchronous_response)
-    bot.add_process(asynchronous_response)
-    #
-    log = bot.logger
-    log.setLevel(logging.INFO)
-    log.addHandler(streamHandler)
+bot = MsBot(verify_jwt_signature=False)
+bot.add_process(respond_to_conversation_update)
+bot.add_process(synchronous_response)
+bot.add_process(asynchronous_response)
+#
+log = bot.logger
+log.setLevel(logging.INFO)
+log.addHandler(streamHandler)
